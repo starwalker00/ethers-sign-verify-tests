97485ef6833c7279cbcb44ef015788d9c2d4ef43
starwalker00 <aaronwalkerup@gmail.com>
2021-11-07 15:36:09 +0100

    script add colors and init wallet

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 var bip39 = require('bip39')
 var crypto = require('crypto')
 
+var { ethers } = require("ethers")
+
+var bgcolor = '\x1b[45m'
+var resetbgcolor = '\x1b[0m'
 
 // random seed
 var  randomBytes = crypto.randomBytes(16)
-console.log(`randomBytes:`)
+console.log(`${bgcolor} randomBytes: ${resetbgcolor}`)
 console.log(randomBytes)
 
 // generate mnemonic
 var mnemonic = bip39.entropyToMnemonic(randomBytes.toString('hex'))
-console.log(`mnemonic:`)
+console.log(`${bgcolor} mnemonic: ${resetbgcolor}`)
 console.log(mnemonic)
+
+var wallet = ethers.Wallet.fromMnemonic(mnemonic)
+console.log(`${bgcolor} wallet: ${resetbgcolor}`)
+console.dir(wallet, {depth: null, colors: true})
