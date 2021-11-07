44b779cefd6ae1a20aa7751f43ddd3306e7c2c64
starwalker00 <aaronwalkerup@gmail.com>
2021-11-07 16:57:08 +0100

    script qrcode : finished

--- appqrcode.mjs
+++ appqrcode.mjs
@@ -1,7 +1,15 @@
 import bip39 from 'bip39'
 import crypto from 'crypto'
 import ethers from 'ethers'
-import QRCode from 'qrcode'
+// QRCode handling
+import QRCodeGenerator from 'qrcode'
+import QRCodeReader from 'qrcode-reader'
+import fs from 'fs'
+import Jimp from 'jimp'
+
+// var Jimp = require("jimp");
+// var fs = require('fs')
+// var QrCode = require('qrcode-reader')
 
 var bgcolor = '\x1b[45m'
 var resetbgcolor = '\x1b[0m'
@@ -41,15 +49,36 @@ console.log(`${bgcolor} expectedSignerAddress === signerAddress ? ${resetbgcolor
 console.log(expectedSignerAddress === signerAddress)
 
 // test qrcode
-// var url = await QRCode.toString('I am a pony!',{type:'terminal'})
+// var url = await QRCodeGenerator.toString('I am a pony!',{type:'terminal'})
 // console.log(url)
 
-// qrcode - encode -public address -tokenid -signedMessage
-var encodedString = [ signerAddress, ';',message, ';',signedMessage];
-var url = await QRCode.toString(encodedString,{type:'terminal'})
+// QRCodeGenerator - encode -public address -tokenid -signedMessage
+var encodedString = [ signerAddress, message, signedMessage ].join(';')
+var url = await QRCodeGenerator.toString(encodedString,{type:'terminal'})
 // console.log(url)
 // write qrcode to file
-await QRCode.toFile('./qrcode.png', encodedString)
+await QRCodeGenerator.toFile('./qrcode.png', encodedString)
+// read QRCode
+var buffer = fs.readFileSync('./qrcode.png')
+var image = await Jimp.read(buffer)
+let qrcode = new QRCodeReader()
+// qrcode-reader's API doesn't support promises, so wrap it
+const value = await new Promise((resolve, reject) => {
+qrcode.callback = (err, v) => err != null ? reject(err) : resolve(v);
+qrcode.decode(image.bitmap);
+});
+var result = value.result
+console.log(result);
+
+// qrcodereader result compared to value used to create it
+console.log(`${bgcolor} qrcodegenerator input (encodedString): ${resetbgcolor}`)
+console.log(encodedString)
+console.log(encodedString.split(';'))
+console.log(`${bgcolor} qrcodereader output (result): ${resetbgcolor}`)
+console.log(result)
+console.log(result.split(';'))
+console.log(`${bgcolor} qrcodegenerator input === qrcodereader output ? ${resetbgcolor}`)
+console.log(encodedString === result)
 
 // verify signer IS NOT the expected signer with ALTERED message - unhappy path
 var alteredMessage = message + 'altered'
